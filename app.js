76a8195ea17fdfee5e1db8c23ab87e47e5c93523
VitaliiHamash <vitalii.m.hamash@gmail.com>
2019-08-03 23:06:36 +0300

    last day change

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Модель
 var model = "AngularApp It's working";
 
-
 // We must create new model!!!
-// var model = Permission;???
-// var Permission ={};???
+/*var model =[{section: "allSectios", permission: {chechOne: true, chechTwo: true, chechThree: false},
+                {section: "calendar", permission: {view: true, edit: true, remove: false}},
+                {section: "profile", permission: {view: true, edit: true, remove: false}},
+                {section: "property", permission: {view: true, edit: true, remove: false}},
+                {section: "contacts", permission: {view: true, edit: true, remove: false}}];
+*/
 
 // Модуль
 var PermissionApp = angular.module("PermissionApp", []);
@@ -12,8 +15,26 @@ var PermissionApp = angular.module("PermissionApp", []);
 
 // Контролер
 PermissionApp.controller("PermissionAppCtrl", function ($scope) {
-    $scope.test = model;
+    $scope.data = model;
 });
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
             // Behavior
             //$scope.clickHandler = function() {
             //$localStorage.key = value;
