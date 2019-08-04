40f985e73c7664c401fc944f17259c1bc0607ea9
VitaliiHamash <vitalii.m.hamash@gmail.com>
2019-08-05 00:12:02 +0300

    work with MVC

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 // Модель
-var model = "AngularApp It's working";
+var model =[{section: "allSectios", permission: {chechOne: false, chechTwo: false, chechThree: false}},
+{section: "calendar", permission: {view: false, edit: false, remove: false}},
+{section: "profile", permission: {view: false, edit: false, remove: false}},
+{section: "property", permission: {view: false, edit: false, remove: false}},
+{section: "contacts", permission: {view: false, edit: false, remove: false}}];
 
-// We must create new model!!!
-/*var model =[{section: "allSectios", permission: {chechOne: true, chechTwo: true, chechThree: false},
-                {section: "calendar", permission: {view: true, edit: true, remove: false}},
-                {section: "profile", permission: {view: true, edit: true, remove: false}},
-                {section: "property", permission: {view: true, edit: true, remove: false}},
-                {section: "contacts", permission: {view: true, edit: true, remove: false}}];
-*/
 
 // Модуль
 var PermissionApp = angular.module("PermissionApp", []);
 
 
 // Контролер
-PermissionApp.controller("PermissionAppCtrl", function ($scope) {
+PermissionApp.controller("CheckboxCtrl", ['$scope',  function ($scope) {
+    
     $scope.data = model;
-});
-            
-        
+
+    //Check All Activation
+    for (var i = 1; i < arr.length; i++){
+        if ($scope.Model[i].permission = {
+        view : true,
+        edit : true,
+        remove : true,
+        }){
+            //Check All 2 and 3 Activation !!!; $scope.checked = true;
+        };
+    }
+}]);
+
 
 
 
