6e694599f1cf43531a694cab351b70330962334f
vitalii.m.hamash@gmail.com <vitalii.m.hamash@gmail.com>
2019-08-05 17:10:09 +0300

    Almostworking

--- app.js
+++ app.js
@@ -14,34 +14,26 @@ var PermissionApp = angular.module("PermissionApp", []);
 PermissionApp.controller("CheckboxCtrl", ['$scope',  function ($scope) {
     
     $scope.CheckboxData = model;
-    $scope.all = false;
+    $scope.CkeckAll = false;
     
     //Check All Activation
-    $scope.changeHandler = function() {
-    
-        for (var i = 1; i < $scope.CheckboxData.length; i++){
-            if ($scope.CheckboxData[i].permission.view == true & $scope.CheckboxData[i].permission.edit == true & $scope.CheckboxData[i].permission.remove == true){
-                $scope.all = true;
-            }else{$scope.all = false;}
+    /*$scope.changeHandler = function() {
+        for (var i = 1; i < CheckboxData.length; i++){
+            if (CheckboxData[i].permission.view == "true" & CheckboxData[i].permission.edit == "true" & CheckboxData[i].permission.remove == "true"){
+                $scope.CkeckAll = true;
+            }
         }
+    };*/
+
+    $scope.changeHandler = function() {
+        if (CheckboxData[1].permission.view == "true")
+        {$scope.CkeckAll = true;};
     };
+
+
+
 }]);
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
             
-
             // Behavior
             //$scope.clickHandler = function() {
             //$localStorage.key = value;
