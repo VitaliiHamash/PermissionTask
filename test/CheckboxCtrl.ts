cc7e1974c8a7a8c06b1116af1d3879ed0803c9a4
vitalii.m.hamash@gmail.com <vitalii.m.hamash@gmail.com>
2019-08-08 17:05:32 +0300

    almost work

---
+++ test/CheckboxCtrl.ts
@@ -0,0 +1,81 @@
+//Model import
+var model = require('./model.js');
+
+// Модуль
+var angular = require('angular');
+var PermissionApp = angular.module("PermissionApp", []);
+
+// Контролер
+ PermissionApp.controller("CheckboxCtrl", ['$scope',  function ($scope) {
+    
+    //view
+    $scope.CheckboxData = model;
+    $scope.CkeckAll = false;
+    
+    //change view AllOne
+    $scope.changeAllOne = function(){
+            if ($scope.CheckboxData[0].permission.chechOne == true){
+                    $scope.CheckboxData[1].permission.view = true;
+                    $scope.CheckboxData[2].permission.view = true;
+                    $scope.CheckboxData[3].permission.view = true;
+                    $scope.CheckboxData[4].permission.view = true;
+            }
+            else{   $scope.CheckboxData[1].permission.view = false;
+                    $scope.CheckboxData[2].permission.view = false;
+                    $scope.CheckboxData[3].permission.view = false;
+                    $scope.CheckboxData[4].permission.view = false;
+                }
+            }
+
+    //change view AllTwo
+    $scope.changeAllTwo = function(){
+            if ($scope.CheckboxData[0].permission.chechTwo == true){
+                    $scope.CheckboxData[1].permission.edit = true;
+                    $scope.CheckboxData[2].permission.edit = true;
+                    $scope.CheckboxData[3].permission.edit = true;
+                    $scope.CheckboxData[4].permission.edit = true;
+            }
+            else{   $scope.CheckboxData[1].permission.edit = false;
+                    $scope.CheckboxData[2].permission.edit = false;
+                    $scope.CheckboxData[3].permission.edit = false;
+                    $scope.CheckboxData[4].permission.edit = false;
+                }
+            }
+
+
+    //change view AllThree
+    $scope.changeAllThree = function(){
+        if ($scope.CheckboxData[0].permission.chechThree == true){
+                $scope.CheckboxData[1].permission.remove = true;
+                $scope.CheckboxData[2].permission.remove = true;
+                $scope.CheckboxData[3].permission.remove = true;
+                $scope.CheckboxData[4].permission.remove = true;
+        }
+        else{   $scope.CheckboxData[1].permission.remove = false;
+                $scope.CheckboxData[2].permission.remove = false;
+                $scope.CheckboxData[3].permission.remove = false;
+                $scope.CheckboxData[4].permission.remove = false;
+            }
+    }
+
+
+    //Check All Activation
+    $scope.changeHandler = function() {
+        var j;
+        for (j in $scope.CheckboxData){
+            if ($scope.CheckboxData[j].permission.view == true && $scope.CheckboxData[j].permission.edit == true && $scope.CheckboxData[j].permission.remove == true){
+                $scope.CkeckAll = true;
+            }
+        }
+    };
+    
+    
+    
+    //Save data to LocalStorage
+     $scope.clickHandler = function(){
+        localStorage.setItem("SaveData", JSON.stringify($scope.CheckboxData));    
+    }
+    
+
+
+}]);
---
+++ test/model.ts
@@ -0,0 +1,6 @@
+// Модель
+exports.moduls =[{section:"allSectios", permission: {chechOne: false, chechTwo: false, chechThree: false}},
+{section: "calendar", permission: {view: false, edit: false, remove: false}},
+{section: "profile", permission: {view: false, edit: false, remove: false}},
+{section: "property", permission: {view: false, edit: false, remove: false}},
+{section: "contacts", permission: {view: false, edit: false, remove: false}}];
